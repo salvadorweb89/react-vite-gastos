b018c0ab789e9645eb03e593a4a5858585045411
salvador.ruiz <salvadorweb89@gmail.com>
2023-08-21 13:59:29 +0200

    - Finalizo animación apertura y cierre de modal. - Implemento campos del formulario de nuevo gasto.

--- react-gastos/src/components/Modal.jsx
+++ react-gastos/src/components/Modal.jsx
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types';
 import IconoCerrarModal from '../img/cerrar.svg';
 
-const Modal = ({setModal}) => {
+const Modal = ({setModal, animarModal, setAnimarModal}) => {
 
 const cerrarModal = () => {
-  setModal(false);
+  setAnimarModal(false);
+
+  setTimeout(() => {
+    setModal(false);
+  }, 500);
 }
 
   return (
@@ -13,13 +17,39 @@ const cerrarModal = () => {
       <img src={IconoCerrarModal} alt="Botón Cerrar Modal de Nuevo Gasto" 
       title="Pulsa para cerrar el modal de nuevo gasto" onClick={cerrarModal} />
       </div>
-      <p>Mostrando modal</p>
+      <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
+        <legend>Nuevo Gasto</legend>
+        <div className="campo">
+          <label htmlFor="nombreGasto">Nombre</label>
+          <input id="nombreGasto"type="text" placeholder="Indica un nombre para el gasto" />
+        </div>
+        <div className="campo">
+          <label htmlFor="importeGasto">Importe</label>
+          <input id="importeGasto"type="number" placeholder="Indica un importe para el gasto. Ej: 300" />
+        </div>
+        <div className="campo">
+          <label htmlFor="categoriaGasto">Categoría</label>
+          <select id="categoriaGasto">
+            <option value="">-- Selecciona</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">suscripciones</option>
+          </select>
+          
+          <input type="submit" value="Añadir gasto" />
+        </div>
+      </form>
     </div>
   );
 }
 
 Modal.propTypes = {
-  setModal: PropTypes.func.isRequired
+  setModal: PropTypes.func.isRequired,
+  animarModal: PropTypes.bool.isRequired,
+  setAnimarModal: PropTypes.func.isRequired,
+
 }
 
 export default Modal;
